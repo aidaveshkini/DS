c54034a245714de6da68a52ee0a75d3f9787196b
Fereshteh-Sadafi-V <frshtsdf1@gmail.com>
2022-12-17 22:31:21 +0330

    postordertrave & postfix to tree ✅

--- expression tree.py
+++ expression tree.py
@@ -1,156 +1,95 @@
+from collections import deque
 
-class ExpressionTree_node:
-    def __init__(self,data=None):
-        self.data = data
-        self.parent = None
-        self.left_child = None
-        self.right_child = None
+# Represents a node of the required tree✅
+class ExprssionTree_node:
+	def __init__(self,data):
+		self.data=data
+		self.left=self.right=None
 class Expression_Tree:
     def __init__(self):
         self.root = None
-    def parInfixExpr_to_tree(self,infixExpr):
-        #we want to make a tree of an infix expression with complete parenthesis
-        #we begin with the first null node:🟥
-        #we iterate the string of expression & make the appropriate node for each character:
-        node = ExpressionTree_node()
-        for i in range (len(infixExpr)):
-            if self.is_operator(infixExpr[i]):
-                node.data = infixExpr[i]
-                #each operator has 1 or 2 operand(s) & is located befor or between them(operands)
-                #so after an operator in string,we meet the right-side operand
-                next_node = ExpressionTree_node()
-                node.right_child = next_node
-                next_node.parent = node
-                node = node.right_child
-            elif (infixExpr[i] == '('):
-                #open parentesis means priority & prior expression goes deeper
-                next_node = ExpressionTree_node()
-                node.left_child = next_node
-                next_node.parent = node
-                node = node.left_child
-            elif (infixExpr[i] == ')'):
-                #close paremtesis means prior expression ends so we go up
-                node = node.parent
-            else: #is operand
-                node.data = infixExpr[i]
-                #each operand in expression is located befor or after an operator
-                #so either its the right-side or left-side operand we go back to its parent node in tree & continue iterating
-                if (not node.parent):
-                #make a null parent node
-                  next_node = ExpressionTree_node()
-                  node.parent = next_node
-                  next_node.left_child = node
-                #go to the parent
-                node = node.parent
-        while (node.parent):
-            node = node.parent
-        self.root = node
-        print("your expression tree is built:)\n")
 
-
-    def is_binaryOpr(self,char):
-        if (char == '+' or char == '-' or char == '*' or char == '/' or char == '^'):
-            return True
-        return False
-
-    def is_unaryOpr(self,char):
-        if (char == 'sin' or char == 'log'):
-            return True
-        return False
-
-    def is_operator(self,char):
-        if (self.is_binaryOpr(char) or self.is_unaryOpr(char)):
-            return True
-        return False
-
-    def preorder_trav(self):
-        node = self.root
-        if node:
-            print(node.data,end=' ')
-            node.left_child.preorder_trav
-            node.right_child.preorder_trav
-        return
-    def inorder_trav(self):
-        node = self.root
-        if node:
-            node.left_child.inorder_trav()
-            print(node.data,end=' ')
-            node.right_child.inorder_trav()
-        return
-    def postorder_trav(self):
-        node = self.root
-        if node:
-            node.left_child.postorder_trav()
-            node.right_child.postorder_trav()
-            print(node.data,end=' ')
-        return
-
-
-
-class stack:
-   def __init__(self):
-      self.arr = []
-   def push(self, data):
-      self.arr.append(data)
-   def pop(self):
-      try:
-         return self.arr.pop(-1)
-      except:
-         pass
-   def top(self):
-      try:
-         return self.arr[-1]
-      except:
-         pass
-   def size(self):
-      return len(self.arr)
-
-
-def postfix_to_tree(a):
-    nodes_stack = stack()
-    for i in range (len(a)):
-        # If the character is an operand
-        if a[i]>='a' and a[i]<='z':
-            nodes_stack.push(a[i])
+# Function to print the infix expression for the tree🟥
+    def inordertraverse(self): #recursion
+        p = self.root
+        if (p == None):
+            return
         else:
-            right = nodes_stack.pop()
-            left = nodes_stack.pop()
-            new_node = ExpressionTree_node(a[i])
-            new_node.left_child = left
-            new_node.right_child = right
-            nodes_stack.push(new_node)
-    TREE = Expression_Tree()
-    TREE.root = nodes_stack.pop()
-    return TREE
+            self.inordertraverse(p.left)
+            print(p.data,end=' ')
+            self.inordertraverse(p.right)
+
+# Function to print the postfix expression for the tree✅
+    def postordertraverse(self): #recursion
+        p = self.root
+        if (p == None):
+            return
+        else:
+            subtree1 = Expression_Tree()
+            subtree1.root= self.root.left
+            subtree2 = Expression_Tree()
+            subtree2.root= self.root.right
+            subtree1.postordertraverse()
+            subtree2.postordertraverse()
+            print(p.data,end=' ')
+
+# Function to print the prefix expression for the tree🟥
+    def preordertraverse(self): #recursion
+        p = self.root
+        if (p == None):
+            return 
+        else:
+            print(p.data,end=' ')
+            self.preordertraverse(p.left)
+            self.preordertraverse(p.right)
+
+def postfix_to_tree(expr):#✅
+ 
+    # base case
+    if not expr:
+        return
+ 
+    # create an empty stack to store tree pointers
+    s = deque()
+ 
+    # traverse the postfix expression
+    for c in expr:
+        # if the current token is an operator
+        if c == '+' or c == '-' or c == '*' or c == '/' or c == '^':
+            # pop two nodes `x` and `y` from the stack
+            x = s.pop()
+            y = s.pop()
+ 
+            # construct a new binary tree whose root is the operator and whose
+            # left and right children point to `y` and `x`
+            Nodee = ExprssionTree_node(c)
+            Nodee.left = y
+            Nodee.right = x
+
+ 
+            # push the current node into the stack
+            s.append(Nodee)
+ 
+        # if the current token is an operand, create a new binary tree node
+        # whose root is the operand and push it into the stack
+        else:
+            s.append(ExprssionTree_node(c))
+ 
+    # a pointer to the root of the expression tree remains on the stack
+    tree = Expression_Tree()
+    tree.root = s[-1]
+    return tree
 
 
 
-#driver code for infixExpr to tree
-bitree2 = Expression_Tree()
-bitree2.parInfixExpr_to_tree("(3+((5+9)*2)")
-bitree2.inorder_trav()
 
 
-bitree = Expression_Tree()
-while True:
-    print('1: prefixExp to tree\n2: parinfixExp to tree\n3: infixExp to tree\n4: postfixExp to tree')
-    print('5: Traverse Preorder\n6: Travers Inorder\n7: Traverse Postorder\n 8: exit\n')
-    select = int(input("choose a number: "))
-    if (select == 2):
-        parinfixExpr = str(input('enter your infix expression: '))
-        bitree.parInfixExpr_to_tree(parinfixExpr)
-    elif (select == 4):
-        postfixExpr = input('enter your postfix expression: ')
-        postfix_to_tree(postfixExpr).postorder_trav()
-    elif (select == 5):
-        bitree.preorder_trav()
-    elif (select == 6):
-        bitree.inorder_trav
-    elif (select == 7):
-        bitree.postorder_trav
-    elif (select == 8):
-        print("    finished;)")
-        break
+tree = prefix
+
+
+
+#tree = postfix_to_tree('ab+')
+#tree.postordertraverse()
 
 
 # (3+((5+9)*2)
